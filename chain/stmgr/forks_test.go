c015dda956a063e9f9cf56cadf58b27ad23995fe
vyzo <vyzo@hackzen.org>
2022-04-12 10:33:38 +0300

    remove manifest from upgrade schedule, get it inside the migration instead

--- chain/stmgr/forks_test.go
+++ chain/stmgr/forks_test.go
@@ -126,7 +126,7 @@ func TestForkHeightTriggers(t *testing.T) {
 			Network: network.Version1,
 			Height:  testForkHeight,
 			Migration: func(ctx context.Context, sm *StateManager, cache MigrationCache, cb ExecMonitor,
-				_ cid.Cid, root cid.Cid, height abi.ChainEpoch, ts *types.TipSet) (cid.Cid, error) {
+				root cid.Cid, height abi.ChainEpoch, ts *types.TipSet) (cid.Cid, error) {
 				cst := ipldcbor.NewCborStore(sm.ChainStore().StateBlockstore())
 
 				st, err := sm.StateTree(root)
@@ -270,7 +270,7 @@ func testForkRefuseCall(t *testing.T, nullsBefore, nullsAfter int) {
 			Expensive: true,
 			Height:    testForkHeight,
 			Migration: func(ctx context.Context, sm *StateManager, cache MigrationCache, cb ExecMonitor,
-				_ cid.Cid, root cid.Cid, height abi.ChainEpoch, ts *types.TipSet) (cid.Cid, error) {
+				root cid.Cid, height abi.ChainEpoch, ts *types.TipSet) (cid.Cid, error) {
 				migrationCount++
 				return root, nil
 			}}}, cg.BeaconSchedule())
@@ -404,7 +404,7 @@ func TestForkPreMigration(t *testing.T) {
 			Network: network.Version1,
 			Height:  testForkHeight,
 			Migration: func(ctx context.Context, sm *StateManager, cache MigrationCache, cb ExecMonitor,
-				_ cid.Cid, root cid.Cid, height abi.ChainEpoch, ts *types.TipSet) (cid.Cid, error) {
+				root cid.Cid, height abi.ChainEpoch, ts *types.TipSet) (cid.Cid, error) {
 
 				// Make sure the test that should be canceled, is canceled.
 				select {
@@ -423,7 +423,7 @@ func TestForkPreMigration(t *testing.T) {
 			PreMigrations: []PreMigration{{
 				StartWithin: 20,
 				PreMigration: func(ctx context.Context, _ *StateManager, cache MigrationCache,
-					_ cid.Cid, _ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
+					_ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
 					wait20.Done()
 					wait20.Wait()
 
@@ -437,7 +437,7 @@ func TestForkPreMigration(t *testing.T) {
 			}, {
 				StartWithin: 20,
 				PreMigration: func(ctx context.Context, _ *StateManager, cache MigrationCache,
-					_ cid.Cid, _ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
+					_ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
 					wait20.Done()
 					wait20.Wait()
 
@@ -451,7 +451,7 @@ func TestForkPreMigration(t *testing.T) {
 			}, {
 				StartWithin: 20,
 				PreMigration: func(ctx context.Context, _ *StateManager, cache MigrationCache,
-					_ cid.Cid, _ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
+					_ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
 					wait20.Done()
 					wait20.Wait()
 
@@ -467,7 +467,7 @@ func TestForkPreMigration(t *testing.T) {
 				StartWithin: 15,
 				StopWithin:  5,
 				PreMigration: func(ctx context.Context, _ *StateManager, cache MigrationCache,
-					_ cid.Cid, _ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
+					_ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
 
 					<-ctx.Done()
 					close(wasCanceled)
@@ -479,7 +479,7 @@ func TestForkPreMigration(t *testing.T) {
 			}, {
 				StartWithin: 10,
 				PreMigration: func(ctx context.Context, _ *StateManager, cache MigrationCache,
-					_ cid.Cid, _ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
+					_ cid.Cid, _ abi.ChainEpoch, _ *types.TipSet) error {
 
 					checkCache(t, cache)
 
