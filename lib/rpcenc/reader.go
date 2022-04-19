d1a76d8e0caf0b8d603ecfcd0eb68371b167b921
Geoff Stuart <geoff.vball@gmail.com>
2022-04-19 14:14:15 -0300

    Update lib/rpcenc/reader.go
    Co-authored-by: Łukasz Magiera <magik6k@users.noreply.github.com>

--- lib/rpcenc/reader.go
+++ lib/rpcenc/reader.go
@@ -15,7 +15,7 @@ import (
 	"sync"
 	"time"
 
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
+	"github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 
 	"github.com/google/uuid"
 	logging "github.com/ipfs/go-log/v2"
@@ -102,7 +102,7 @@ func ReaderParamEncoder(addr string) jsonrpc.Option {
 	return jsonrpc.WithParamEncoder(new(io.Reader), func(value reflect.Value) (reflect.Value, error) {
 		r := value.Interface().(io.Reader)
 
-		if r, ok := r.(*sealing.NullReader); ok {
+		if r, ok := r.(*nullreader.NullReader); ok {
 			return reflect.ValueOf(ReaderStream{Type: Null, Info: fmt.Sprint(r.N)}), nil
 		}
 
@@ -411,7 +411,7 @@ func ReaderParamDecoder() (http.HandlerFunc, jsonrpc.ServerOption) {
 				return reflect.Value{}, xerrors.Errorf("parsing null byte count: %w", err)
 			}
 
-			return reflect.ValueOf(sealing.NewNullReader(abi.UnpaddedPieceSize(n))), nil
+			return reflect.ValueOf(nullreader.NewNullReader(abi.UnpaddedPieceSize(n))), nil
 		}
 
 		u, err := uuid.Parse(rs.Info)
